719ee3367c694b84188eff9fad2676be55210e91
Rajasekhar0707 <53522979+Rajasekhar0707@users.noreply.github.com>
2023-09-04 22:20:21 +0530

    Update HelloWorld.java for Poll SCM

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello Rajasekhar!");
+    System.out.println("Hello Rajasekhar Editing code to check Poll SCM which is check regular interval for new commits if present it'll trigger the build!");
   }
 }
