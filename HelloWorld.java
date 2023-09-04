86813f413f289820a02ea4a2359a06d7c1ead53e
Rajasekhar0707 <53522979+Rajasekhar0707@users.noreply.github.com>
2023-09-04 23:06:56 +0530

    Update HelloWorld.java with http://52.55.41.214:8080/github-webhook/

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello Rajasekhar Checking Web hook test happens or not !");
+    System.out.println("Hello Rajasekhar Checking Web hook using http://52.55.41.214:8080/github-webhook/ !");
   }
 }
